d29f1437ea438976084e71cb61e06ebfd9732fcc
mohammed.elkouch <mohammed.elkouch@it.devcorp.fr>
2022-08-31 15:08:31 +0100

    fix: listen port

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
-import { AuthModule } from './auth/auth.module';
-
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
