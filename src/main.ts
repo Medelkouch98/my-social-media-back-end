1edcee88990c8e273802ce09b64f7e1fb9f2ced3
mohammed.elkouch <mohammed.elkouch@it.devcorp.fr>
2022-09-06 18:17:46 +0100

    feat: add message status

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { TransformInterceptor } from './interceptors/transform.interceptor';
 
+const port = parseInt(process.env.PORT);
+const globalPrefix = 'api';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  app.enableCors();
+  app
+    .setGlobalPrefix(globalPrefix)
+    .useGlobalInterceptors(new TransformInterceptor())
+    .useGlobalPipes(
+      new ValidationPipe({
+        transform: true,
+        whitelist: true,
+        forbidNonWhitelisted: true,
+      }),
+    );
+  await app.listen(port);
+  Logger.log(`🚀 Application is running on: http://localhost:${port}`);
 }
 bootstrap();
